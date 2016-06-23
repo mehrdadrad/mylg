031b5c21ba8a2eedd4b6c5fe983381fe9c313280
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-06-22 21:45:52 -0700

    init cli

---
+++ cli/cli.go
@@ -0,0 +1,33 @@
+package cli
+
+import (
+	"gopkg.in/readline.v1"
+)
+
+type Readline struct {
+	instance *readline.Instance
+}
+
+func Init(prompt string) *Readline {
+	var (
+		r   Readline
+		err error
+	)
+	r.instance, err = readline.New(prompt + "> ")
+	if err != nil {
+		panic(err)
+	}
+	return &r
+}
+
+func (r *Readline) Run() {
+	func() {
+		for {
+			line, err := r.instance.Readline()
+			if err != nil { // io.EOF, readline.ErrInterrupt
+				break
+			}
+			println(line)
+		}
+	}()
+}
