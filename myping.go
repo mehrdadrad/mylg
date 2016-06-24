183cd7d5dcaaa8d03489d1405424c560e9df9b76
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-06-24 10:54:07 -0700

    init connect

--- myping.go
+++ myping.go
@@ -4,9 +4,20 @@ import (
 	"github.com/mehrdadrad/myping/cli"
 	"github.com/mehrdadrad/myping/icmp"
 	"regexp"
+	"strings"
 )
 
+func validateProvider(p string) (string, error) {
+	p = strings.ToLower(p)
+	return p, nil
+}
+
 func main() {
+	var (
+		err       error
+		cProvider string
+	)
+
 	rep := make(chan string, 1)
 	cmd := make(chan string, 1)
 	nxt := make(chan struct{}, 1)
@@ -14,7 +25,7 @@ func main() {
 	c := cli.Init("local")
 	go c.Run(cmd, nxt)
 
-	r, _ := regexp.Compile("(ping|trace) (.*)")
+	r, _ := regexp.Compile("(ping|connect) (.*)")
 
 	for {
 		select {
@@ -28,9 +39,19 @@ func main() {
 			switch subReq[1] {
 			case "ping":
 				p := icmp.NewPing()
-				p.AddIP(subReq[2])
+				p.IP(subReq[2])
 				p.Ping(rep)
 				println(<-rep)
+				nxt <- struct{}{}
+			case "connect":
+				var provider string
+				if provider, err = validateProvider(subReq[2]); err != nil {
+					println("provider not available")
+					nxt <- struct{}{}
+					continue
+				}
+				cProvider = provider
+				c.SetPrompt(cProvider)
 				nxt <- struct{}{}
 			}
 		}
