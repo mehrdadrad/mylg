2c9dd7f378a710cf6f5302858988169b38fa9f15
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-06-29 15:07:29 -0700

    update completer, remove item

--- cli/cli.go
+++ cli/cli.go
@@ -3,10 +3,12 @@ package cli
 import (
 	"github.com/mehrdadrad/mylg/banner"
 	"gopkg.in/readline.v1"
+	"strings"
 )
 
 type Readline struct {
-	instance *readline.Instance
+	instance  *readline.Instance
+	completer *readline.PrefixCompleter
 }
 
 func Init(prompt string) *Readline {
@@ -24,6 +26,7 @@ func Init(prompt string) *Readline {
 			readline.PcItem("help"),
 		)
 	)
+	r.completer = completer
 	r.instance, err = readline.NewEx(&readline.Config{
 		Prompt:          prompt + "> ",
 		HistoryFile:     "/tmp/myping",
@@ -36,6 +39,38 @@ func Init(prompt string) *Readline {
 	}
 	banner.Println()
 	return &r
+}
+
+func (r *Readline) RemoveItemCompleter(pcItem string) {
+	child := []readline.PrefixCompleterInterface{}
+	for _, p := range r.completer.Children {
+		if strings.TrimSpace(string(p.GetName())) != pcItem {
+			child = append(child, p)
+		}
+	}
+
+	r.completer.Children = child
+
+}
+
+func (r *Readline) UpdateCompleter(pcItem string, pcSubItems map[string]string) {
+	child := []readline.PrefixCompleterInterface{}
+	var pc readline.PrefixCompleter
+	for _, p := range r.completer.Children {
+		if strings.TrimSpace(string(p.GetName())) == pcItem {
+			c := []readline.PrefixCompleterInterface{}
+			for item, _ := range pcSubItems {
+				c = append(c, readline.PcItem(item))
+			}
+			pc.Name = []rune("node ")
+			pc.Children = c
+			child = append(child, &pc)
+		} else {
+			child = append(child, p)
+		}
+	}
+
+	r.completer.Children = child
 }
 
 func (r *Readline) SetPrompt(p string) {
