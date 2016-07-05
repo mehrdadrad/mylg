eefc4779d3a109baab98007b9d4f006a290ca77a
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-04 23:05:06 -0700

    fixed lint issues

--- lg/telia.go
+++ lg/telia.go
@@ -1,3 +1,5 @@
+// Package lg provides looking glass methods for selected looking glasses
+// Telia Carrier Looking Glass ASN 1299
 package lg
 
 import (
@@ -8,16 +10,16 @@ import (
 	"regexp"
 )
 
+// A Telia represents a telia looking glass request
 type Telia struct {
 	Host string
 	IPv  string
 	Node string
 }
 
-var (
-	teliaDefaultNode string = "Los Angeles"
-)
+var teliaDefaultNode = "Los Angeles"
 
+// Set configures host and ip version
 func (p *Telia) Set(host, version string) {
 	p.Host = host
 	p.IPv = version
@@ -25,9 +27,13 @@ func (p *Telia) Set(host, version string) {
 		p.Node = teliaDefaultNode
 	}
 }
+
+// GetDefaultNode returns telia default node
 func (p *Telia) GetDefaultNode() string {
 	return teliaDefaultNode
 }
+
+// GetNodes returns all Telia nodes (US and International)
 func (p *Telia) GetNodes() []string {
 	var nodes []string
 	for node := range p.FetchNodes() {
@@ -35,9 +41,14 @@ func (p *Telia) GetNodes() []string {
 	}
 	return nodes
 }
+
+// ChangeNode set new requested node
 func (p *Telia) ChangeNode(node string) {
 	p.Node = node
 }
+
+// Ping tries to connect Telia's ping looking glass through HTTP
+// Returns the result
 func (p *Telia) Ping() (string, error) {
 	resp, err := http.PostForm("http://looking-glass.telia.net/",
 		url.Values{"query": {"ping"}, "protocol": {p.IPv}, "addr": {p.Host}, "router": {p.Node}})
@@ -50,10 +61,11 @@ func (p *Telia) Ping() (string, error) {
 	b := r.FindStringSubmatch(string(body))
 	if len(b) > 0 {
 		return b[1], nil
-	} else {
-		return "", errors.New("error")
 	}
+	return "", errors.New("error")
 }
+
+//FetchNodes returns all available nodes through HTTP
 func (p *Telia) FetchNodes() map[string]string {
 	var nodes = make(map[string]string, 100)
 	resp, err := http.Get("http://looking-glass.telia.net/")
