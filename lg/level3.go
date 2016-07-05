b43bd5614bc7f73381e2d5f1dcc68ccfe76af7a2
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-04 23:11:19 -0700

    fixed lint issues

--- lg/level3.go
+++ lg/level3.go
@@ -1,3 +1,5 @@
+// Package lg provides looking glass methods for selected looking glasses
+// Level3 Carrier Looking Glass ASN 3356
 package lg
 
 import (
@@ -9,6 +11,7 @@ import (
 	"strings"
 )
 
+// A Level3 represents a telia looking glass request
 type Level3 struct {
 	Host  string
 	IPv   string
@@ -17,14 +20,16 @@ type Level3 struct {
 }
 
 var (
-	level3Nodes              = map[string]string{"Los Angeles, CA": "ear1.lax1"}
-	level3DefaultNode string = "Los Angeles, CA"
+	level3Nodes       = map[string]string{"Los Angeles, CA": "ear1.lax1"}
+	level3DefaultNode = "Los Angeles, CA"
 )
 
 func sanitize(b string) string {
 	re := regexp.MustCompile("<(.*)>")
 	return re.ReplaceAllString(b, "")
 }
+
+// Set configures host and ip version
 func (p *Level3) Set(host, version string) {
 	p.Host = host
 	p.IPv = version
@@ -33,9 +38,13 @@ func (p *Level3) Set(host, version string) {
 		p.Node = level3DefaultNode
 	}
 }
+
+// GetDefaultNode returns telia default node
 func (p *Level3) GetDefaultNode() string {
 	return level3DefaultNode
 }
+
+// GetNodes returns all level3 nodes (US and International)
 func (p *Level3) GetNodes() []string {
 	level3Nodes = p.FetchNodes()
 	var nodes []string
@@ -44,9 +53,14 @@ func (p *Level3) GetNodes() []string {
 	}
 	return nodes
 }
+
+// ChangeNode set new requested node
 func (p *Level3) ChangeNode(node string) {
 	p.Node = node
 }
+
+// Ping tries to connect Level3's ping looking glass through HTTP
+// Returns the result
 func (p *Level3) Ping() (string, error) {
 	resp, err := http.PostForm("http://lookingglass.level3.net/ping/lg_ping_output.php",
 		url.Values{"count": {p.Count}, "size": {"64"}, "address": {p.Host}, "sitename": {level3Nodes[p.Node]}})
@@ -59,11 +73,11 @@ func (p *Level3) Ping() (string, error) {
 	b := r.FindStringSubmatch(strings.Replace(string(body), "<br>", "\n", -1))
 	if len(b) > 0 {
 		return sanitize(b[1]), nil
-	} else {
-		return "", errors.New("error")
 	}
+	return "", errors.New("error")
 }
 
+//FetchNodes returns all available nodes through HTTP
 func (p *Level3) FetchNodes() map[string]string {
 	var nodes = make(map[string]string, 100)
 	resp, err := http.Get("http://lookingglass.level3.net/ping/lg_ping_main.php")
