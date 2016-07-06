b1db47b3cad5a2e1bd1222e2af3994fccdcf3d3d
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-06 08:39:30 -0700

    added asn validation

--- ripe/ripe.go
+++ ripe/ripe.go
@@ -14,14 +14,24 @@ type ASN struct {
 }
 
 // GetData gets ASN information from RIPE NCC
-func (a *ASN) GetData() {
+func (a *ASN) GetData() bool {
+	if len(a.Number) < 2 {
+		println("error: AS number invalid")
+		return false
+	}
 	resp, err := http.Get("https://stat.ripe.net/data/as-overview/data.json?resource=AS" + a.Number)
 	if err != nil {
 		println(err)
+		return false
+	}
+	if resp.StatusCode != 200 {
+		println("error: check your AS number")
+		return false
 	}
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(body, &a.Data)
+	return true
 }
 
 // PrettyPrint print ASN information (holder)
