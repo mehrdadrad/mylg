433eafdb3613fd2a484e2eb433a471c86fa76753
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-05 20:21:34 -0700

    fixed comments

--- ripe/ripe.go
+++ ripe/ripe.go
@@ -1,3 +1,4 @@
+// Package ripe provides ASN and IP information
 package ripe
 
 import (
@@ -6,15 +7,13 @@ import (
 	"net/http"
 )
 
+// ASN represents ASN information
 type ASN struct {
 	Number string
 	Data   map[string]interface{}
 }
 
-func flag() {
-
-}
-
+// GetData gets ASN information from RIPE NCC
 func (a *ASN) GetData() {
 	resp, err := http.Get("https://stat.ripe.net/data/as-overview/data.json?resource=AS" + a.Number)
 	if err != nil {
@@ -25,6 +24,7 @@ func (a *ASN) GetData() {
 	json.Unmarshal(body, &a.Data)
 }
 
+// PrettyPrint print ASN information (holder)
 func (a *ASN) PrettyPrint() {
 	data, ok := a.Data["data"].(map[string]interface{})
 	if ok {
