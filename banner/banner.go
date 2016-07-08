098f01c6e7b596a3213b4cbdef677f7c56ad9653
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-07 18:14:56 -0700

    changed banner width

--- banner/banner.go
+++ banner/banner.go
@@ -1,18 +1,23 @@
 // Package banner provides banner information
 package banner
 
+import (
+	"fmt"
+)
+
 // Println print out banner information
 func Println() {
 	b := `
- =======================	
-             _    ___ 
-   _ __ _  _| |  / __|
-  | '  \ || | |_| (_ |
-  |_|_|_\_, |____\___|
-        |__/          
- 	
-     My Looking Glass 
- ====== myLG v0.1.3 ======
+================================================	
+                          _    ___ 
+                _ __ _  _| |  / __|
+               | '  \ || | |_| (_ |
+               |_|_|_\_, |____\___|
+                      |__/          
+	
+                 My Looking Glass
+                  http://mylg.io
+================== myLG v0.1.5 =================
 	`
-	println(b)
+	fmt.Println(b)
 }
