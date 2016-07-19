72f79cf44955aeced5504a312a1187b139ca2561
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-18 18:46:15 -0700

    init http ping package

---
+++ http/ping/ping.go
@@ -0,0 +1,78 @@
+// Package ping tries to ping a HTTP server through different ways
+// Connection, Session (Head), Get and Post
+package ping
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"time"
+)
+
+// Ping represents HTTP ping request
+type Ping struct {
+	url     string
+	timeout time.Duration
+	conn    net.Conn
+}
+
+// NewPing validate and constructs request object
+func NewPing(URL string, timeout time.Duration) *Ping {
+	u, err := url.Parse(URL)
+	if err != nil {
+
+	}
+	_, err = net.ResolveIPAddr("ip", u.Host)
+	if err != nil {
+
+	}
+	return &Ping{
+		url:     URL,
+		timeout: timeout,
+	}
+}
+
+// ping tries to create a TCP connection
+func (p *Ping) ping() (float64, bool) {
+	var (
+		err   error
+		delta float64
+		sTime time.Time
+	)
+
+	sTime = time.Now()
+	p.conn, err = net.DialTimeout("tcp", p.url, p.timeout*time.Second)
+	delta = time.Since(sTime).Seconds()
+	if err != nil {
+		print(err.Error())
+		return 0, false
+	}
+	p.conn.Close()
+	return delta, true
+}
+
+// pingConn tries number of connection
+func (p *Ping) pingConn() {
+	for i := 0; i < 4; i++ {
+		if t, ok := p.ping(); ok {
+			fmt.Printf("HTTP Connection seq=%d, time=%.3f ms\n", i, t*1000)
+		} else {
+			fmt.Println("HTTP Connection seq=%d, timeout", i)
+		}
+	}
+}
+
+// pingSession tries to have multiple sessions
+// per a connection
+func (p *Ping) pingSession() {
+
+}
+
+// pingHead tries to execute head command
+func (p *Ping) pingHead() {
+
+}
+
+func (p *Ping) Run() {
+	p.pingConn()
+}
