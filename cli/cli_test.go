df01c16ba6ce8ab9eb31fb3d69086d7381e9d1df
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-20 16:33:13 -0700

    added flag test

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -24,3 +24,42 @@ func TestSetPrompt(t *testing.T) {
 		t.Error("Set cli prompt failed")
 	}
 }
+
+func TestFlag(t *testing.T) {
+	// test help
+	url, flag := cli.Flag("www.mylg.io help")
+	if _, ok := flag["help"]; !ok {
+		t.Error("flag expected help request but not exist")
+	}
+	// test simple arg
+	url, flag = cli.Flag("www.mylg.io")
+	if _, ok := flag["help"]; ok {
+		t.Error("flag expected none help")
+	}
+	if url != "www.mylg.io" {
+		t.Error("flag unexpected url")
+	}
+	// test option and string value
+	url, flag = cli.Flag("www.mylg.io -option=value")
+	if _, ok := flag["option"]; !ok {
+		t.Error("flag unexpected option")
+	} else if flag["option"] != "value" {
+		t.Error("flag unexpected string value")
+	}
+	if url != "www.mylg.io" {
+		t.Error("")
+		println("url", url)
+	}
+	// test option and int value
+	url, flag = cli.Flag("www.mylg.io -option=1976")
+	if _, ok := flag["option"]; !ok {
+		t.Error("flag unexpected option")
+	} else if flag["option"] != 1976 {
+		t.Error("flag unexpected int value")
+	}
+	if url != "www.mylg.io" {
+		t.Error("")
+		println("url", url)
+	}
+
+}
