45c3951bab144fb3752ed06b3b5eb3cd5e9b7854
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-21 18:49:22 -0700

    init whois package

---
+++ whois/whois.go
@@ -0,0 +1,47 @@
+// Package whois tries to get information about
+// IP address / Prefix / Domain (todo)
+package whois
+
+import (
+	"github.com/mehrdadrad/mylg/ripe"
+)
+
+// whois represents whois providers
+type whois interface {
+	Set(r string)
+	GetData() bool
+	PrettyPrint()
+}
+
+var (
+	w = map[string]whois{"asn": new(ripe.ASN), "prefix": new(ripe.Prefix)}
+)
+
+// Lookup tries to get whois information
+// ASN and prefix/ip information
+func Lookup(args string) {
+	if ripe.IsASN(args) {
+		w["asn"].Set(args)
+		w["asn"].GetData()
+		w["asn"].PrettyPrint()
+	} else if ripe.IsIP(args) || ripe.IsPrefix(args) {
+		w["prefix"].Set(args)
+		w["prefix"].GetData()
+		w["prefix"].PrettyPrint()
+	} else {
+		help()
+	}
+}
+
+// help represents whois help
+func help() {
+	println(`
+    usage:
+          whois ASN/CIDR/IPAddress
+
+    Example:
+          whois 8.8.8.8
+          whois 8.0.0.0/8
+          whois 577		  
+	`)
+}
