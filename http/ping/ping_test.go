1ae5ffe2747e0cbc80e914cb7d7068262034b639
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-07-22 16:42:04 -0700

    more test funcs

--- http/ping/ping_test.go
+++ http/ping/ping_test.go
@@ -4,7 +4,62 @@ import (
 	"testing"
 
 	"github.com/mehrdadrad/mylg/http/ping"
+	"gopkg.in/h2non/gock.v0"
 )
+
+func TestNewPing(t *testing.T) {
+	url, _ := ping.NewPing("help")
+	if url != nil {
+		t.Error("NewPing expected nil but returned string")
+	}
+	_, err := ping.NewPing("garbage")
+	if err == nil {
+		t.Error("Newping expected error but it didn't return")
+	}
+}
+
+func TestPingGet(t *testing.T) {
+	var url = "mylg.io"
+	gock.New(url).
+		Reply(200)
+
+	p, _ := ping.NewPing(url)
+	r, _ := p.PingGet()
+	if r.StatusCode != 200 {
+		t.Error("PingGet expected to get 200 but didn't")
+	}
+	if r.TotalTime == 0 {
+		t.Error("PingGet expected to set totaltime but it didn't")
+	}
+}
+func TestPingPost(t *testing.T) {
+	var url = "mylg.io"
+	gock.New(url).
+		Reply(200)
+
+	p, _ := ping.NewPing(url)
+	r, _ := p.PingPost()
+	if r.StatusCode != 200 {
+		t.Error("PingPost expected to get 200 but didn't")
+	}
+	if r.TotalTime == 0 {
+		t.Error("PingPost expected to set totaltime but it didn't")
+	}
+}
+func TestPingHead(t *testing.T) {
+	var url = "mylg.io"
+	gock.New(url).
+		Reply(200)
+
+	p, _ := ping.NewPing(url)
+	r, _ := p.PingHead()
+	if r.StatusCode != 200 {
+		t.Error("PingHead expected to get 200 but didn't")
+	}
+	if r.TotalTime == 0 {
+		t.Error("PingHead expected to set totaltime but it didn't")
+	}
+}
 
 func TestNormalize(t *testing.T) {
 	n := ping.Normalize("google.com")
