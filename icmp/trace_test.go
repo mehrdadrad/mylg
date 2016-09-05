10f927347ca59f33edf20b00b5083239b1aad5b3
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-09-05 01:50:15 -0700

    added config to trace test

--- icmp/trace_test.go
+++ icmp/trace_test.go
@@ -3,11 +3,13 @@ package icmp_test
 import (
 	"testing"
 
+	"github.com/mehrdadrad/mylg/cli"
 	"github.com/mehrdadrad/mylg/icmp"
 )
 
 func TestNewTrace(t *testing.T) {
-	_, err := icmp.NewTrace("google.com -n -nr -m 30")
+	cfg, _ := cli.ReadDefaultConfig()
+	_, err := icmp.NewTrace("google.com -n -nr -m 30", cfg)
 	if err != nil {
 		t.Error("unexpected error. expected %v, actual %v", nil, err)
 	}
