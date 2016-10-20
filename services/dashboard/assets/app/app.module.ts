064b8f25efef90385fe4545c20ca5787e6ff6238
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-10-20 00:00:37 -0700

    replaced new name style

--- services/dashboard/assets/app/app.module.ts
+++ services/dashboard/assets/app/app.module.ts
@@ -1,27 +1,35 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule }  from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HttpModule, JsonpModule } from '@angular/http';
 
-import { PingComponent } from './ping/ping';
-import { HomeComponent } from './home/home';
+import { PingComponent } from './ping/ping.component';
+import { TraceComponent } from './trace/trace.component';
+import { HomeComponent } from './home/home.component';
+import { AgGridModule } from '../node_modules/ag-grid-ng2/main';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
         RouterModule.forRoot([
-			{ path: 'ping', component: PingComponent },
-			{ path: 'home', component: HomeComponent },
+			{ path: 'ping',  component: PingComponent  },
+			{ path: 'trace', component: TraceComponent },
+			{ path: 'home',  component: HomeComponent  },
 			{ path: '', pathMatch: 'full', redirectTo:'/ping' },
 		],{ useHash: true }),
-		BrowserModule
+		BrowserModule,
+        AgGridModule.withNg2ComponentSupport(),
+        HttpModule,
+        JsonpModule,
   ],
-  declarations: [ 
+  declarations: [
 		AppComponent,
 		PingComponent,
+		TraceComponent,
 		HomeComponent
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
   })
 
-export class AppModule { }
+export class AppModule {}
