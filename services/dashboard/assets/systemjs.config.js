995e19205d0f7b47cf28efbea3338265da76476b
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-10-21 21:41:34 -0700

    update npm

--- services/dashboard/assets/systemjs.config.js
+++ services/dashboard/assets/systemjs.config.js
@@ -19,8 +19,8 @@
     },
     paths: {
       // paths serve as alias
-	  //'npm:': 'node_modules/'
-      'npm:': 'https://unpkg.com/'
+	  'npm:': 'node_modules/'
+      //'npm:': 'https://unpkg.com/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -43,7 +43,8 @@
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
       'ts': 'npm:plugin-typescript/lib/plugin.js',
       'typescript': 'npm:typescript/lib/typescript.js',
-
+      'ag-grid-ng2': 'node_modules/ag-grid-ng2',
+      'ag-grid': 'node_modules/ag-grid'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
