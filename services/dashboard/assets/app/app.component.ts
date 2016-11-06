757e0246312e1dbcf0dbb9150c131fa73f24f230
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-11-05 22:39:59 -0700

    clean up

--- services/dashboard/assets/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'mylg-app',
-  template: `<router-outlet></router-outlet>`,
-  directives: [RouterOutlet]
-})
-export class AppComponent { }
--- services/dashboard/assets/app/main.ts
+++
@@ -1,8 +0,0 @@
-import {enableProdMode} from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-enableProdMode();
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
