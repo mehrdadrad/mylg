441043274330732424d4a41c299c03e9244eac24
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2016-11-29 08:56:39 -0800

    add TCP.SYN scan

--- scan/scan.go
+++ scan/scan.go
@@ -2,6 +2,7 @@
 package scan
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"net"
@@ -13,6 +14,10 @@ import (
 	"sync"
 	"time"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+
 	"github.com/mehrdadrad/mylg/cli"
 	"github.com/olekukonko/tablewriter"
 )
@@ -22,6 +27,16 @@ type Scan struct {
 	minPort int
 	maxPort int
 	target  string
+	lport   int
+	rport   int
+	laddr   net.IP
+	raddr   net.IP
+	ifName  string
+	network string
+	forceV4 bool
+	forceV6 bool
+	synScan bool
+	ip      gopacket.NetworkLayer
 }
 
 // NewScan creats scan object
@@ -38,6 +53,10 @@ func NewScan(args string, cfg cli.Config) (Scan, error) {
 		help(cfg)
 		return scan, fmt.Errorf("")
 	}
+
+	scan.forceV4 = cli.SetFlag(flag, "4", true).(bool)
+	scan.forceV6 = cli.SetFlag(flag, "6", false).(bool)
+	scan.synScan = cli.SetFlag(flag, "s", false).(bool)
 
 	pRange := cli.SetFlag(flag, "p", cfg.Scan.Port).(string)
 
@@ -61,21 +80,17 @@ func NewScan(args string, cfg cli.Config) (Scan, error) {
 		return scan, err
 	}
 
-	if !scan.IsCIDR() {
-		ipAddr, err := net.ResolveIPAddr("ip", scan.target)
-		if err != nil {
-			return scan, err
-		}
-		scan.target = ipAddr.String()
-	} else {
+	if scan.IsCIDR() {
 		return scan, fmt.Errorf("it doesn't support CIDR")
 	}
+
+	scan.setIP()
 
 	return scan, nil
 }
 
 // IsCIDR checks the target if it's CIDR
-func (s Scan) IsCIDR() bool {
+func (s *Scan) IsCIDR() bool {
 	_, _, err := net.ParseCIDR(s.target)
 	if err != nil {
 		return false
@@ -83,30 +98,263 @@ func (s Scan) IsCIDR() bool {
 	return true
 }
 
-// Run tries to scan wide range ports (TCP)
-func (s Scan) Run() {
-	if !s.IsCIDR() {
-		host(s.target, s.minPort, s.maxPort)
-	}
+func isIPv4(ip net.IP) bool {
+	return len(ip.To4()) == net.IPv4len
 }
 
-// host tries to scan a single host
-func host(ipAddr string, minPort, maxPort int) {
+func isIPv6(ip net.IP) bool {
+	if r := strings.Index(ip.String(), ":"); r != -1 {
+		return true
+	}
+	return false
+}
+
+func (s *Scan) setIP() error {
+	ips, err := net.LookupIP(s.target)
+	if err != nil {
+		return err
+	}
+	for _, ip := range ips {
+		if isIPv4(ip) && !s.forceV6 {
+			s.raddr = ip
+			s.network = "ip4"
+			break
+		} else if isIPv6(ip) && !s.forceV4 {
+			s.raddr = ip
+			s.network = "ip6"
+			break
+		}
+	}
+	return nil
+}
+
+// Run tries to scan wide range ports (TCP)
+func (s *Scan) Run() {
 	var (
-		wg      sync.WaitGroup
-		tStart  = time.Now()
-		counter int
+		openPorts []int
+		err       error
 	)
 
-	var ports []int
+	fmt.Printf("Scan %s (%s)\n", s.target, s.raddr)
+
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Protocol", "Port", "Status"})
 
-	for i := minPort; i <= maxPort; i++ {
+	tStart := time.Now()
+	if !s.synScan {
+		openPorts = s.tcpConnScan()
+	} else {
+		openPorts, err = s.tcpSYNScan()
+	}
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	for _, p := range openPorts {
+		table.Append([]string{"TCP", fmt.Sprintf("%d", p), "Open"})
+	}
+	table.Render()
+
+	if len(openPorts) == 0 {
+		println("there isn't any opened port")
+	} else {
+		elapsed := fmt.Sprintf("%.3f seconds", time.Since(tStart).Seconds())
+		println("Scan done:", len(openPorts), "opened port(s) found in", elapsed)
+	}
+
+}
+
+func (s *Scan) packetDataTCP(rport int) (error, []byte) {
+	tcp := &layers.TCP{
+		SrcPort: layers.TCPPort(s.lport),
+		DstPort: layers.TCPPort(rport),
+		Seq:     1,
+		SYN:     true,
+		Window:  15000,
+	}
+
+	tcp.SetNetworkLayerForChecksum(s.ip)
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		ComputeChecksums: true,
+		FixLengths:       true,
+	}
+
+	if err := gopacket.SerializeLayers(buf, opts, tcp); err != nil {
+		return err, []byte{}
+	}
+
+	return nil, buf.Bytes()
+}
+
+func (s *Scan) setProto(proto string) error {
+	var netProto = fmt.Sprintf("%s:%s", s.network, proto)
+
+	switch netProto {
+	case "ip4:tcp":
+		s.ip = &layers.IPv4{
+			SrcIP:    s.laddr,
+			DstIP:    s.raddr,
+			Protocol: layers.IPProtocolTCP,
+		}
+	case "ip4:udp":
+		s.ip = &layers.IPv4{
+			SrcIP:    s.laddr,
+			DstIP:    s.raddr,
+			Protocol: layers.IPProtocolUDP,
+		}
+	case "ip6:tcp":
+		s.ip = &layers.IPv6{
+			SrcIP:      s.laddr,
+			DstIP:      s.raddr,
+			NextHeader: layers.IPProtocolTCP,
+		}
+	case "ip6:udp":
+		s.ip = &layers.IPv6{
+			SrcIP:      s.laddr,
+			DstIP:      s.raddr,
+			NextHeader: layers.IPProtocolUDP,
+		}
+
+	}
+
+	return nil
+}
+
+func (s *Scan) setLocalNet() error {
+	conn, err := net.Dial("udp", net.JoinHostPort(s.raddr.String(), "80"))
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	if lAddr, ok := conn.LocalAddr().(*net.UDPAddr); ok {
+		s.laddr = lAddr.IP
+		s.lport = lAddr.Port
+	} else {
+		return fmt.Errorf("can not find local address/port")
+	}
+
+	ifs, _ := net.Interfaces()
+	for _, i := range ifs {
+		addrs, _ := i.Addrs()
+		for _, addr := range addrs {
+			ip, _, _ := net.ParseCIDR(addr.String())
+			if ip.String() == s.laddr.String() {
+				s.ifName = i.Name
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *Scan) tcpSYNScan() ([]int, error) {
+	var err error
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	if err = s.setLocalNet(); err != nil {
+		println(err.Error())
+	}
+	if err = s.setProto("tcp"); err != nil {
+		println(err.Error())
+	}
+	go func() {
+		if err := s.sendTCPSYN(); err != nil {
+			println(err.Error())
+		}
+		cancel()
+	}()
+
+	openPorts, err := s.pCapture(ctx)
+	return openPorts, err
+}
+
+func (s *Scan) pCapture(ctx context.Context) ([]int, error) {
+	var (
+		tcp       *layers.TCP
+		timeout   = 100 * time.Nanosecond
+		openPorts []int
+		ok        bool
+	)
+	handle, err := pcap.OpenLive(s.ifName, 6*1024, false, timeout)
+	if err != nil {
+		return nil, err
+	}
+	defer handle.Close()
+
+	filter := "(tcp or udp) and src host " + s.raddr.String()
+	if err := handle.SetBPFFilter(filter); err != nil {
+		return nil, err
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+LOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			break LOOP
+		case packet := <-packetSource.Packets():
+			tcpLayer := packet.Layer(layers.LayerTypeTCP)
+			if tcpLayer != nil {
+				if tcp, ok = tcpLayer.(*layers.TCP); !ok {
+					continue
+				}
+				if tcp.SYN && tcp.ACK {
+					p, _ := strconv.Atoi(fmt.Sprintf("%d", tcp.SrcPort))
+					openPorts = append(openPorts, p)
+				}
+				continue
+			}
+		}
+	}
+	//TODO: unique .....
+	sort.Ints(openPorts)
+	return openPorts, nil
+}
+
+func (s *Scan) sendTCPSYN() error {
+	var (
+		buf  []byte
+		err  error
+		conn net.PacketConn
+	)
+	if s.network != "ip6" {
+		conn, err = net.ListenPacket(s.network+":tcp", "0.0.0.0")
+	} else {
+		conn, err = net.ListenPacket(s.network+":tcp", "[::]")
+	}
+	if err != nil {
+		return err
+	}
+	for i := s.minPort; i <= s.maxPort; i++ {
+		if err, buf = s.packetDataTCP(i); err != nil {
+			return err
+		}
+		if _, err := conn.WriteTo(buf, &net.IPAddr{IP: s.raddr}); err != nil {
+			println(err.Error())
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+	time.Sleep(2 * time.Second)
+	return nil
+}
+
+// tcpConnScan tries to scan a single host
+func (s *Scan) tcpConnScan() []int {
+	var wg sync.WaitGroup
+
+	var ports []int
+	for i := s.minPort; i <= s.maxPort; i++ {
 		wg.Add(1)
 		go func(i int) {
 			for {
-				conn, err := net.DialTimeout("tcp", net.JoinHostPort(ipAddr, fmt.Sprintf("%d", i)), 2*time.Second)
+				host := net.JoinHostPort(s.raddr.String(), fmt.Sprintf("%d", i))
+				conn, err := net.DialTimeout("tcp", host, 2*time.Second)
 				if err != nil {
 					if strings.Contains(err.Error(), "too many open files") {
 						// random back-off
@@ -119,26 +367,14 @@ func host(ipAddr string, minPort, maxPort int) {
 				conn.Close()
 				break
 			}
-			counter++
 			ports = append(ports, i)
 			wg.Done()
 		}(i)
 	}
 
 	wg.Wait()
-
 	sort.Ints(ports)
-	for i := range ports {
-		table.Append([]string{"TCP", fmt.Sprintf("%d", ports[i]), "Open"})
-	}
-	table.Render()
-
-	if counter == 0 {
-		println("there isn't any opened port")
-	} else {
-		elapsed := fmt.Sprintf("%.3f seconds", time.Since(tStart).Seconds())
-		println("Scan done:", counter, "opened port(s) found in", elapsed)
-	}
+	return ports
 }
 
 // help represents guide to user
